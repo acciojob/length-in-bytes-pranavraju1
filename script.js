d29a9c66ef308f5a284d2326f76ce6c9a72ef1d2
Pranav Raju <120582474+pranavraju1@users.noreply.github.com>
2023-10-08 11:33:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const byteSize = (str) => {
   // write your code here
+	let l = new TextEncoder().encode(str).length;
+	// let l = str.length
+	return l;
 };
 
-// Do not change the code below
+// // Do not change the code below
 const str = prompt("Enter some string.");
-alert(byteSize(str));
+alert(byteSize(str));      
